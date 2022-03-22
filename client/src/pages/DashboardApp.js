bf8e16d499d90dcd940d2c5fd727d7a7a18e96ee
nambook445 <nambook445@naver.com>
2022-03-22 17:21:25 +0900

    이제 대쉬보드를 만들어보자

--- client/src/pages/DashboardApp.js
+++ client/src/pages/DashboardApp.js
@@ -1,5 +1,6 @@
 // material
 import { Box, Grid, Container, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
 // components
 import Page from '../components/Page';
 import {
@@ -20,11 +21,13 @@ import {
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
+  const {isLogin} = useSelector((state) => state.isLogined)
+  const {user} = useSelector((state) => state.userData)
   return (
     <Page title="Dashboard | Minimal-UI">
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
-          <Typography variant="h4">Hi, Welcome back</Typography>
+          {isLogin ? (<Typography variant="h4">{user.nickname}</Typography>) : (<Typography variant="h4">비회원</Typography>)}
         </Box>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
