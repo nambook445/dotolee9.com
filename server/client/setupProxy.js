46dfe73c5f33e6588625ab215c096f6b5f1c9aa0
nambook445 <nambook445@naver.com>
2022-04-07 13:56:13 +0900

    http://3.34.189.123/ 로 경로변경

--- server/client/setupProxy.js
+++ server/client/setupProxy.js
@@ -3,7 +3,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://localhost:8080',
+      target: 'http://3.34.189.123/',
+      changeOrigin: true
+    })
+  );
+  app.use(
+    createProxyMiddleware('/user', {
+      target: 'http://3.34.189.123/',
       changeOrigin: true
     })
   );
