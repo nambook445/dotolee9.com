8830746741094659884e82d66e86caf42e44ab93
nambook445 <nambook445@naver.com>
2022-04-07 17:09:25 +0900

    포트 8080

--- server/client/setupProxy.js
+++ server/client/setupProxy.js
@@ -2,14 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api', {
-      target: 'http://3.34.189.123/',
-      changeOrigin: true
-    })
-  );
-  app.use(
-    createProxyMiddleware('/user', {
-      target: 'http://3.34.189.123/',
+    createProxyMiddleware(['/api', '/user'], {
+      target: 'http://3.34.189.123:8080/',
       changeOrigin: true
     })
   );
