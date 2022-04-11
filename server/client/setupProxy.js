63ff4679c7b5acc76d957637e58a8e15c5d48543
nambook445 <nambook445@naver.com>
2022-04-11 13:56:51 +0900

    dotolee9.com 도메인 적용

--- server/client/setupProxy.js
+++ server/client/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(['/api', '/user'], {
-      target: 'http://52.78.43.223:8080/',
+      target: 'http://dotolee9.com/',
       changeOrigin: true
     })
   );
